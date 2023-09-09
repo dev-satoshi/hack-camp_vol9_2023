2363626323bafbd364fd177330c20e65e45b6948
satoshi <dev.satoshi.1016@gmail.com>
2023-09-09 19:39:43 +0900

    add: カスタムユーザーを作成

--- app/config/settings.py
+++ app/config/settings.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,6 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# カスタムユーザーモデル
+AUTH_USER_MODEL = "accounts.CustomUser"
